4e31e5986d247e3acb2905cff2fb6f767ad25919
chinmay <cmpatil@outlook.in>
2024-06-01 16:08:37 +0530

    dummy_counter

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_pyamnihc_dummy_counter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,60 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+
+  assign uio_oe = 8'b1111_1111;
+  // sync
+  reg [3:0] count_en_p;
+  reg count_en_r;
+  wire count_en_in;
+  assign count_en_in = ui_in[0];
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        count_en_p <= 'b0;
+        count_en_r <= 'b0;
+    end else begin
+        {count_en_r, count_en_p} <= {count_en_p, count_en_in};
+    end
+  end
+  
+  // debounce
+  reg count_en_db_r;
+  reg [3:0] counter_db_r;
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        count_en_db_r <= 'b0;
+        counter_db_r <= 'b0;
+    end else begin
+        if (count_en_db_r != count_en_r) begin
+            counter_db_r <= counter_db_r + 1;
+        end else begin
+            counter_db_r <= 'b0;
+        end
+        if (counter_db_r == 4'b1111) begin
+            count_en_db_r <= count_en_r;
+        end else begin
+            count_en_db_r <= count_en_db_r;
+        end
+    end
+  end
+  
+  reg [15:0] counter_r;
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        counter_r <= 'b0;
+    end else begin
+        if (count_en_db_r) begin
+            counter_r <= counter_r + 1'b1;    
+        end else begin
+            counter_r <= counter_r;
+        end
+    end
+  end
+
+  assign uo_out = counter_r[7:0];
+  assign uio_out = counter_r[15:8];
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, ui_in[7:1], uio_in};
 
 endmodule
